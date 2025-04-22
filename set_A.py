a9f6cc070b0c03c1dcf77ec1359dc383d1c09151
Kenji <erronnmadelo@gmail.com>
2025-04-22 16:40:05 +0800

    dongkoy commit

--- set_A.py
+++ set_A.py
@@ -14,18 +14,33 @@ items = {
 
 cart = []
 flag = 1
+#while loop para na naay flag variable
 while flag == 1:
     print("Welcome to the Sisigan Store! Here are the items available:")
     for item, price in items.items():
-        print(f"{item}: ${price}")
+        print(f"{item}: ₱{price}")
     print("Enter the item you want to buy (or type 'done' to finish):")
     input_item = input()
     if input_item == "done":
+
+        #resibo 
+        print("----------------Thank you for shopping with us! Here are the items in your cart:----------------")
+        for item, quantity in cart:
+            print(f"{item}: {quantity} pcs")
+        print("Your total is:")
+        total = sum(items[item] * quantity for item, quantity in cart)
+        print(f"₱{total}")
+        print("------------------------------------------Please pay at the counter.------------------------------------------")
         break
+
+
     elif input_item in items:
         print(f"How many {input_item} do you want to buy?")
-        quantity = int(input())
-        if quantity > 0:
+        quantity = input()
+        if not isinstance(quantity, int):
+            print("-----------------Please enter an integer to indicate an amount.-----------------")
+        
+        elif quantity > 0:
             cart.append((input_item, quantity))
             print(f"{quantity} {input_item}(s) added to your cart.")
             print(f"Your cart now contains:")
